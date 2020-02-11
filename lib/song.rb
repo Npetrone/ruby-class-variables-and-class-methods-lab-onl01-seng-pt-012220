48c00f63ae5d4b4c75cfae7f0316a836ef317e62
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-11 00:50:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,12 +7,15 @@ class Song
   @@genres = [] 
   
   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1
   end
   
   
-  def artists
-    @@artists << artists
+  def self.artist
+    @@artists
   end
   
   
