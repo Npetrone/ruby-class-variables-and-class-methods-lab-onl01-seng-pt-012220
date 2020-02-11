46de837227ae9d2ad462cefe210cb9aeffbeae81
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-11 00:44:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,23 +3,25 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
-  @@artist = 0
+  @@artists = []
+  @@genres = [] 
   
   def initialize(name, artist, genre)
     @name = name
-    @artist = artist
-    @genre = genre
+    @artists = artists
+    @genres = genre
     @@count += 1
   end
   
   
-  def self.artist
-    
+  def artists
+    @@artists << artists
+  end
   
   
   
   def self.genre
-
+    @@genres
   end
   
   
